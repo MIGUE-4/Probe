0c0de3de220b71bd67e2aa67ce79b6c0fbb63526
Miguel C <miguel.cubillas.c@gmail.com>
2023-08-02 19:38:56 -0500

    Endpoints Terminados

--- main.py
+++ main.py
@@ -16,11 +16,6 @@ with open(r'steam_games.json') as file:
 data = pd.DataFrame(data)
 
 
-@app.get("/index")
-def index():
-
-    return 'prueba'
-
 class fecha_gen(BaseModel):
    year : str
 
@@ -28,9 +23,8 @@ class fecha_gen(BaseModel):
 @app.post("/genero")
 def genero(fecha:fecha_gen):
    
-   return {f"El año de {fecha.year} tuvo estos 5 géneros más vendidos":
-           data[data['release_date'].str[0:4] == fecha.year]['genres'].value_counts().head(5).index.to_list()
-           }
+   return data[data['release_date'].str[0:4] == fecha.year]['genres'].value_counts().head(5).index.to_list()
+        
 
 
 class juegos_year(BaseModel):
@@ -39,5 +33,37 @@ class juegos_year(BaseModel):
 @app.post("/juegos")
 def genero(juego:juegos_year):
    
-   return {f"Juegos lanzados en {juego.year}":
-           data[data['release_date'].str[0:4] == '2010']['title'].unique().tolist()}
+   return data[data['release_date'].str[0:4] == juego.year]['title'].unique().tolist()
+
+class specs_year(BaseModel):
+   year : str
+
+@app.post("/specs")
+def specs(spec:specs_year):
+   
+   return data[data['release_date'].str[0:4] == spec.year].specs.value_counts().head(5).index.tolist()
+
+class early_year(BaseModel):
+   year : str
+
+@app.post("/earlyacces")
+def specs(early:early_year):
+   
+   return data[data['release_date'].str[0:4] == early.year].early_access.value_counts().loc[True]
+   
+
+class metascore_year(BaseModel):
+   year : str
+
+@app.post("/metascore")
+def specs(early:metascore_year):
+    return data[data['release_date'].str[0:4] == early.year].sort_values('metascore', ascending=False).head(5)['title'].tolist()
+
+class sentiment_year(BaseModel):
+   year : str
+
+@app.post("/sentiment")
+def specs(sent:sentiment_year):
+    return data[data['release_date'].str[0:4] == sent.year]['sentiment'].value_counts().to_dict()
+    
+
